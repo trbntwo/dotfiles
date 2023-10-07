cb50d196ab075e1108b358da5a015af43df98572
trbntwo <git@trbntwo.dev>
2023-10-07 16:57:50 +0200

    fix: symlinks linking to nothing

--- setup.sh
+++ setup.sh
@@ -8,21 +8,21 @@
 if [ -f ~/.bashrc ]; then
 	rm ~/.bashrc
 fi
-ln -sf ./scripts/bashrc.sh ~/.bashrc
+ln -sf ~/.dotfiles/scripts/bashrc.sh ~/.bashrc
 
 # templates
 templates(){
-	ln -sf ./templates/bash-script.sh ~/$TEMPLATE_DIRECTORY/bash-script.sh
-	ln -sf ./templates/config-file.conf ~/$TEMPLATE_DIRECTORY/config-file.conf
-	ln -sf ./templates/desktop-entry.desktop ~/$TEMPLATE_DIRECTORY/desktop-entry.desktop
-	ln -sf ./templates/file ~/$TEMPLATE_DIRECTORY/file
-	ln -sf ./templates/json-file.json ~/$TEMPLATE_DIRECTORY/json-file.json
-	ln -sf ./templates/markdown-file.md ~/$TEMPLATE_DIRECTORY/markdown-file.md
-	ln -sf ./templates/systemd-service.service ~/$TEMPLATE_DIRECTORY/systemd-service.service
-	ln -sf ./templates/systemd-timer.timer ~/$TEMPLATE_DIRECTORY/systemd-timer.timer
-	ln -sf ./templates/text-file.txt ~/$TEMPLATE_DIRECTORY/text-file.txt
-	ln -sf ./templates/toml-file.toml ~/$TEMPLATE_DIRECTORY/toml-file.toml
-	ln -sf ./templates/yaml-file.yaml ~/$TEMPLATE_DIRECTORY/yaml-file.yaml
+	ln -sf ~/.dotfiles/templates/bash-script.sh ~/$TEMPLATE_DIRECTORY/bash-script.sh
+	ln -sf ~/.dotfiles/templates/config-file.conf ~/$TEMPLATE_DIRECTORY/config-file.conf
+	ln -sf ~/.dotfiles/templates/desktop-entry.desktop ~/$TEMPLATE_DIRECTORY/desktop-entry.desktop
+	ln -sf ~/.dotfiles/templates/file ~/$TEMPLATE_DIRECTORY/file
+	ln -sf ~/.dotfiles/templates/json-file.json ~/$TEMPLATE_DIRECTORY/json-file.json
+	ln -sf ~/.dotfiles/templates/markdown-file.md ~/$TEMPLATE_DIRECTORY/markdown-file.md
+	ln -sf ~/.dotfiles/templates/systemd-service.service ~/$TEMPLATE_DIRECTORY/systemd-service.service
+	ln -sf ~/.dotfiles/templates/systemd-timer.timer ~/$TEMPLATE_DIRECTORY/systemd-timer.timer
+	ln -sf ~/.dotfiles/templates/text-file.txt ~/$TEMPLATE_DIRECTORY/text-file.txt
+	ln -sf ~/.dotfiles/templates/toml-file.toml ~/$TEMPLATE_DIRECTORY/toml-file.toml
+	ln -sf ~/.dotfiles/templates/yaml-file.yaml ~/$TEMPLATE_DIRECTORY/yaml-file.yaml
 }
 
 case $LANG in
@@ -42,7 +42,7 @@ esac
 if [ ! -d ~/.ssh ]; then
 	mkdir ~/.ssh
 fi
-ln -sf ./ssh/config ~/.ssh/config
+ln -sf ~/.dotfiles/ssh/config ~/.ssh/config
 
 ######################
 ## $XDG_CONFIG_HOME ##
@@ -52,10 +52,10 @@ ln -sf ./ssh/config ~/.ssh/config
 if [ ! -d ~/.config/git ]; then
 	mkdir ~/.config/git
 fi
-ln -sf ./config/git/config ~/.config/git/config
+ln -sf ~/.dotfiles/config/git/config ~/.config/git/config
 
 # yt-dlp
 if [ ! -d ~/.config/yt-dlp ]; then
 	mkdir ~/.config/yt-dlp
 fi
-ln -sf ./config/yt-dlp/config ~/.config/yt-dlp/config
+ln -sf ~/.dotfiles/config/yt-dlp/config ~/.config/yt-dlp/config
