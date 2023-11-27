8aefefbc1998af8c6e9f8fc84266d5ac669498da
trbntwo <git@trbntwo.dev>
2023-11-27 10:17:25 +0100

    feat: add custom prompt

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -15,9 +15,26 @@ export PATH
 # Uncomment the following line if you don't like systemctl's auto-paging feature:
 # export SYSTEMD_PAGER=
 
+
+############
+## prompt ##
+############
+
+PS1='\[\e[38;5;92m\]\u@\h\[\e[0m\]:\[\e[38;5;208m\]\w\[\e[0m\]: '
+
+
+#############
+## aliases ##
+#############
+
 if [ -f ~/.dotfiles/scripts/aliases.sh ]; then
 	. ~/.dotfiles/scripts/aliases.sh
 fi
+
+
+#################
+## environment ##
+#################
 
 if [ -f ~/.dotfiles/scripts/env.sh ]; then
 	. ~/.dotfiles/scripts/env.sh
